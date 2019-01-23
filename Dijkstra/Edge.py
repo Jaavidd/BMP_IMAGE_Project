4d045036b2c65b22f3de728a10b2aadad9b3f41f
Javid Mammadli <37974777+Jaavidd@users.noreply.github.com>
2019-01-23 21:23:28 +0400

    Delete Edge.py

--- Dijkstra/Edge.py
+++
@@ -1,7 +0,0 @@
-
-class Edge(object):
-
-    def __init__(self,weight,startVertex,targetVertex):
-        self.weight=weight
-        self.startVertex=startVertex
-        self.targetVertex=targetVertex
